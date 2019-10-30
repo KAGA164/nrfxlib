a767f77e325aa1e92527da053880032c70c0f272
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2019-10-30 10:32:34 +0100

    Dynamic clock

--- nfc/src/nfc_platform_zephyr.c
+++ nfc/src/nfc_platform_zephyr.c
@@ -12,23 +12,28 @@
 
 #define LOG_MODULE_NAME nfc_platform
 #define LOG_LEVEL CONFIG_NFC_PLATFORM_LOG_LEVEL
-LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL);
 
 struct device *clock;
 
+static void clock_handler(struct device *dev, void *user_data)
+{
+	/* Activate NFCT only when HFXO is running */
+	nrfx_nfct_state_force(NRFX_NFCT_STATE_ACTIVATED);
+}
+
+static struct clock_control_async_data clock_ctrl = {
+	.cb = clock_handler
+};
+
 nrfx_err_t nfc_platform_setup(void)
 {
-	int err;
-
 	clock = device_get_binding(DT_INST_0_NORDIC_NRF_CLOCK_LABEL "_16M");
 	__ASSERT_NO_MSG(clock);
 
-	err = clock_control_on(clock, (void *)1);
-	__ASSERT_NO_MSG(!err);
-
-	IRQ_DIRECT_CONNECT(NFCT_IRQn, CONFIG_NFCT_IRQ_PRIORITY,
+	IRQ_DIRECT_CONNECT(NFCT_IRQn, 0,
 			   nrfx_nfct_irq_handler, 0);
-	IRQ_DIRECT_CONNECT(TIMER4_IRQn, CONFIG_NFCT_IRQ_PRIORITY,
+	IRQ_DIRECT_CONNECT(TIMER4_IRQn, 0,
 			   nrfx_timer_4_irq_handler, 0);
 
 	LOG_DBG("NFC platform initialized");
@@ -38,14 +43,17 @@ nrfx_err_t nfc_platform_setup(void)
 
 void nfc_platform_event_handler(nrfx_nfct_evt_t const *event)
 {
+	int err = 0;
 	switch (event->evt_id) {
 	case NRFX_NFCT_EVT_FIELD_DETECTED:
 		LOG_DBG("Field detected");
-		/* Activate NFCT only when HFXO is running */
-		nrfx_nfct_state_force(NRFX_NFCT_STATE_ACTIVATED);
+		err = clock_control_async_on(clock, (void *)1, &clock_ctrl);
+		__ASSERT_NO_MSG(!err);
 		break;
 
 	case NRFX_NFCT_EVT_FIELD_LOST:
+		err = clock_control_off(clock, (void *)1);
+		__ASSERT_NO_MSG(!err);
 		LOG_DBG("Field lost");
 		break;
 
